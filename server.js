49f7831c6d3bb51b36949d58ca7e0b1e77d03512
Tara Dunmore <30328414+qualmless@users.noreply.github.com>
2021-02-16 13:07:50 -0600

    installed nodemon, created form and dash pages

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 
+
+// ========================
+// get
+// ========================
+
+app.get('/', (req, res) => {
+    res.sendFile('/Users/student/Documents/Documents - STUSD1040/dev/ticketCapstone/index.html')
+    // res.sendFile('/Users/student/Documents/Documents-STUSD1040/dev/selfEXPRESSion/index.html')
+})
+
+
+
 // ========================
 // Listen
 // ========================
-const port = 3000
+const port = 9999
 app.listen(port, function(){
     console.log(`listening on port ${port}`)
 })
